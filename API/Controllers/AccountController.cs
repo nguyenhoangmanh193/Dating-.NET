708c6f262baef2e680411062022f4a6ba7ef2ff0
hoang.nguyenmanh <hoang.nguyenmanh@ncc.asia>
2025-08-20 02:22:54 +0700

    Add Authentication

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entites;
+using API.Extentions;
+using API.Interfaces;
+using Company.ClassLibrary1;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await EmailExists(registerDto.Email))
+            {
+                return BadRequest("Email exitst");
+            }
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            return user.ToDto(tokenService);
+ 
+
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDto.Email);
+            if (user is null)
+            {
+                return Unauthorized("Email not found");
+            }
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (var i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Password invalid");
+            }
+
+            return user.ToDto(tokenService);
+ 
+        }
+
+
+        private async Task<bool> EmailExists(string email)
+        {
+            return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+
+        }
+    }
+}
---
+++ API/DTOs/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Company.ClassLibrary1;
+
+public class LoginDto
+{
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+}
