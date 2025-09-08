3942cb68df05272066f40b5c5bc558f59b05a8b5
hoang.nguyenmanh <hoang.nguyenmanh@ncc.asia>
2025-09-09 01:43:23 +0700

    Update resource

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,5 +1,8 @@
+using System.Security.Claims;
 using API.Data;
+using API.DTOs;
 using API.Entites;
+using API.Extensions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -17,7 +20,7 @@ namespace API.Controllers
             return Ok(await memberRepository.GetMembersAsync());
         }
 
-        
+
         [HttpGet("{id}")]
         public async Task<ActionResult<Member>> GetMemberbyId(string id)
         {
@@ -33,7 +36,31 @@ namespace API.Controllers
         {
             return Ok(await memberRepository.GetPhotosForMemberAsync(id));
         }
-        
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
+        {
+            var memberId = User.GetMemberId();
+
+            var member = await memberRepository.GetMemberForUpdate(memberId);
+
+            if (member == null) return BadRequest("Cound not get member");
+
+            member.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
+            member.Description = memberUpdateDto.Description ?? member.Description;
+            member.City = memberUpdateDto.City ?? member.City;
+            member.Country = memberUpdateDto.Country ?? member.Country;
+
+            member.User.DisplayName = memberUpdateDto.DisplayName ?? member.User.DisplayName;
+
+            memberRepository.Update(member);
+
+            if (await memberRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update member");
+
+        }
+
 
     }
 }
