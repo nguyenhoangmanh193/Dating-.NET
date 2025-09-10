1882bea5741eb30a633e8f5540e9825bc5bb9ad2
hoang.nguyenmanh <hoang.nguyenmanh@ncc.asia>
2025-09-11 03:27:54 +0700

    Reactive Form

--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -41,7 +41,10 @@ export class MemberPhotos implements OnInit{
       next: photo => {
         this.memberService.editMode.set(false);
         this.loading.set(false);
-        this.photos.update(photos => [...photos, photo])
+        this.photos.update(photos => [...photos, photo]);
+        if( !this.memberService.member()?.imageUrl){
+          this.setMainLocalPhoto(photo);
+        }
       },
       error: error => {
         console.log("Error uploading image: ", error);
@@ -53,13 +56,7 @@ export class MemberPhotos implements OnInit{
   setMainPhoto(photo: Photo){
      this.memberService.setMainPhoto(photo).subscribe({
       next: () => {
-           const currentUser = this.accoutService.currentUser();
-           if (currentUser) currentUser.imageUrl = photo.url;
-           this.accoutService.setCurrentUser(currentUser as User);
-           this.memberService.member.update(member => ({
-             ...member,
-             imageUrl: photo.url
-           }) as Member)
+           this.setMainLocalPhoto(photo)
       }
      })
   }
@@ -70,5 +67,15 @@ export class MemberPhotos implements OnInit{
          this.photos.update(photos => photos.filter(x => x.id != photoId))
       }
     })
+  }
+
+  private setMainLocalPhoto(photo: Photo){
+    const currentUser = this.accoutService.currentUser();
+           if (currentUser) currentUser.imageUrl = photo.url;
+           this.accoutService.setCurrentUser(currentUser as User);
+           this.memberService.member.update(member => ({
+             ...member,
+             imageUrl: photo.url
+           }) as Member)
   }
 }
